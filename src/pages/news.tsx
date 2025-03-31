55a93870e8f87cb46c21f09aed0947e3cd438371
FlxdeCat <132283385+FlxdeCat@users.noreply.github.com>
2025-03-31 23:47:55 +0700

    continue news

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -1,5 +1,6 @@
 import { Footer } from "@/components/footer"
 import { Navbar } from "@/components/navbar"
+import { Button } from "@/components/ui/button"
 
 function News() {
 
@@ -14,10 +15,20 @@ function News() {
   return (
     <div className="m-0 p-0 flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex flex-col items-center flex-1">
+      <main className="flex flex-col items-center flex-1 mx-16">
         <div className="flex flex-col items-center w-full max-w-5xl mt-4">
-          <div className="text-2xl font-bold text-start w-full">Recent News</div>
-
+          <div className="text-2xl font-bold text-start w-full mb-6">Recent News</div>
+          <div className="flex flex-col space-y-4 w-full">
+            <div className="flex justify-between w-full gap-4">
+              <img src="/holocure.png" alt="Holocure" className="max-w-[200px] aspect-[3/2] object-cover rounded-sm" />
+              <div className="h-full w-full text-start flex flex-col gap-2">
+                <div className="font-bold text-xl">title</div>
+                <div className="text-sm text-muted-foreground">date</div>
+                <div>news</div>
+              </div>
+              <Button>asd</Button>
+            </div>
+          </div>
         </div>
       </main>
       <Footer />
